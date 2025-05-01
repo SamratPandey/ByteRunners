8361b426a4abc71975fd4d9ebc1c2edd16916c5a
SamratPandey <samratpandeyofficial02@gmail.com>
2025-05-01 19:45:06 +0530

    Update the Nav section

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -168,9 +168,7 @@ const Nav = (props) => {
             aria-label="Notifications"
           >
             <FontAwesomeIcon icon={faBell} />
-            <Badge className="absolute -top-1 -right-1 bg-green-500 text-black text-xs h-4 min-w-4 flex items-center justify-center p-0">
-              2
-            </Badge>
+          
           </button>
 
           {!isLoggedIn ? (
